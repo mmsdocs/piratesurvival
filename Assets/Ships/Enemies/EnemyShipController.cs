409a68aade828c4cb636f770ce82768621b4df2c
Marcelo Santos <marcelo.santos.eci@gmail.com>
2024-01-18 10:50:51 -0300

    feat: finally a IA enemy with Navigation AI

--- Assets/Ships/Enemies/EnemyShipController.cs
+++ Assets/Ships/Enemies/EnemyShipController.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class EnemyShipController : ShipController
 {
+    private NavMeshAgent _agent;
+
     [Header("Enemy Ship Specs")]
     public float speed = 0.0f;
     public float safeDistanceFromPlayer = 0.0f;
@@ -11,31 +14,42 @@ public class EnemyShipController : ShipController
     protected Transform playerTransform;
     protected SpriteRenderer spriteRenderer;
 
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+        _agent.speed = speed;
+    }
+
     protected void Move()
     {
-        if (s_Rigidbody != null && canMove && isDistanceSafeFromPlayer())
+        if (s_Rigidbody == null) return;
+        if (!canMove) return;
+        if (isDistanceSafeFromPlayer())
         {
-            s_Rigidbody.velocity = s_Rigidbody.transform.up * speed;
+            _agent.isStopped = false;
+            if (playerTransform != null) _agent.SetDestination(playerTransform.position);
         }
+        else _agent.isStopped = true;
     }
 
     protected void LookPlayer()
     {
-        if (playerTransform != null)
-        {
-            float angle = Utils.AngleBetween(transform.position, playerTransform.position, Vector2.up);
-            s_Rigidbody.MoveRotation(angle);
-        }
+        if (_agent == null) return;
+        
+        float angle = Utils.AngleBetween(transform.position, _agent.steeringTarget, Vector2.up);
+        s_Rigidbody.MoveRotation(angle);
        
     }
 
     protected override void NextShipState()
     {
-        if (shipStates.Length > 0 && currentShipState < shipStates.Length)
-        {
-            currentShipState++;
+        if (shipStates.Length < 0) return;
+     
+        currentShipState++;
+        if (currentShipState < shipStates.Length) 
             spriteRenderer.sprite = shipStates[currentShipState];
-        }
     }
 
     protected override void SetShipState(int state)
